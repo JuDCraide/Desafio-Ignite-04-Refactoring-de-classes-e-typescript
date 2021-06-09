55fe9d12fc7bf4e13f8ee512b015e8f4e738c80c
JúDCraide <juliadcraide31@gmail.com>
2021-06-08 23:40:56 -0300

    Migrando componente Food

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from 'react';
 
 import Header from '../../components/Header';
-import api from '../../services/api';
-import Food from '../../components/Food';
+import Food, { FoodInterface } from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
+import api from '../../services/api';
+
 import { FoodsContainer } from './styles';
 
-interface FoodInterface {
-  id: number;
-  name: string;
-}
 
 const Dashboard = () => {
   const [foods, setFoods] = useState<FoodInterface[]>([]);
